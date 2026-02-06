dddb007a560ddecf786dfd458fd160012835f750
Adrián <adrian2000gg@gmail.com>
2026-02-06 19:28:50 +0100

    Agrego las dependencias de navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,13 @@ plugins {
 
 android {
     namespace = "com.example.animaciones"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
 
     defaultConfig {
         applicationId = "com.example.animaciones"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -50,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
